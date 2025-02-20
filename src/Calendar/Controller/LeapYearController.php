72ee379fdfc431c601ae828c3cd603c112a310fb
Ilias Rais <73229695+artfull-boy@users.noreply.github.com>
2025-02-20 09:56:28 +0100

    Added Dependency Injection

--- src/Calendar/Controller/LeapYearController.php
+++ src/Calendar/Controller/LeapYearController.php
@@ -8,15 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class LeapYearController
 {
-    public function index(Request $request, int $year): Response
+    public function index(int $year): string
     {
         $leapYear = new LeapYear();
         if ($leapYear->isLeapYear($year)) {
-            $response =  new Response('Yep, this is a leap year!');
+            return 'Yep, this is a leap year! ';
         }
-        else {$response = new Response('Nope, this is not a leap year.');}
-        $response->setTtl(10);
-        return $response;
-    }
 
+        return 'Nope, this is not a leap year.';
+    }
 }
--- web/front.php
+++ web/front.php
@@ -2,47 +2,13 @@
 // example.com/web/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Simplex\Framework;
-use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Simplex\GoogleListener;
-use Symfony\Component\HttpKernel;
 
-
-function render_template(Request $request): Response
-{
-    extract($request->attributes->all(), EXTR_SKIP);
-    ob_start();
-    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-    return new Response(ob_get_clean());
-}
+$routes = include __DIR__.'/../src/app.php';
+$container = include __DIR__.'/../src/container.php';
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-
-
-
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new Simplex\ContentLengthListener());
-$dispatcher->addSubscriber(new GoogleListener());
-
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-$framework = new Simplex\Framework($dispatcher, $matcher, $controllerResolver, $argumentResolver);
-$framework = new HttpKernel\HttpCache\HttpCache(
-    $framework,
-    new HttpKernel\HttpCache\Store(__DIR__.'/../cache')
-);
-
-$response = $framework->handle($request);
+$response = $container->get('framework')->handle($request);
 
 $response->send();
